a67e3eb953dadffb68786e8a1f7a7ce1ce33c5c9
verus-carver <pranshubahadur@gmail.com>
2023-09-30 19:15:24 +0530

    fix MSR

--- modules.py
+++ modules.py
@@ -159,8 +159,8 @@ class MultiScaleRetention(Layer):
 
     def call(self, x):
         W = self.wg(x)
-        x = tf.split(x, self.hdim, -1)
-        x = tf.concat([self.heads[i](x[i], x[i], x[i]) for i in range(self.heads)], -1)
+        x = tf.split(x, self.hdim, 2)
+        x = tf.concat([self.heads[i]([x[i], x[i], x[i]]) for i in range(self.heads)], -1)
         Y = self.gn(x)
         x = self.wo(W * Y)
         return x
