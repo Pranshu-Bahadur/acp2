e08a2a917107d5bf63e86c67a89ee7dbb61124c0
root <pranshubahadur@gmail.com>
2023-10-03 21:05:23 +0000

    test

--- modules.py
+++ modules.py
@@ -65,17 +65,14 @@ class FeedForward(tf.keras.layers.Layer):
   def __init__(self, d_model, dff, dropout_rate=0.1):
     super().__init__()
     self.seq = tf.keras.Sequential([
-      tf.keras.layers.Dense(dff),
-      tf.keras.layers.Dense(d_model, activation='relu'),
+      tf.keras.layers.Dense(dff, activation='gelu'),
+      tf.keras.layers.Dense(d_model),
       tf.keras.layers.Dropout(dropout_rate)
     ])
-    self.add = tf.keras.layers.Add()
-    self.layer_norm = tf.keras.layers.LayerNormalization()
 
   def call(self, x):
-    x = self.add([x, self.seq(x)])
-    x = self.layer_norm(x)
-    return x
+    return self.seq(x)
+
 
 
 class EncoderLayer(tf.keras.layers.Layer):
@@ -247,6 +244,6 @@ class RetentionBlock(Layer):
         self.msr = MultiScaleRetention(dim, hdim, seq_len, retention_layer=retention_layer)
 
     def call(self, x, training=False):
-      msr_x = self.msr(x)
-      x = self.ffn(msr_x)
+      msr_x = self.msr(self.layer_norm(x)) + x
+      x = self.ffn(self.layer_norm(msr_x)) + msr_x
       return x
